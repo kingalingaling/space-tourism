351ea6245833a9a8b4b12de28390cb4a10105d22
Anthonia Alexis Asikpo <anthonia@nugitech.com>
2023-04-18 13:49:28 +0100

    added menu icon

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/header';
 function App() {
 
   return (
-    <section className='flex flex-col bg-home-lg sm:bg-home-md md:bg-home-lg bg-no-repeat bg-cover text-[#fff] w-screen h-screen pt-4'>
+    <section className='flex flex-col bg-home-sm md:bg-home-md lg:bg-home-lg bg-no-repeat bg-cover text-[#fff] w-screen h-screen pt-4'>
       <Header />
       <Explore />
     </section>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import Logo from "../assets/images/icons/logo.svg";
+import Menu from "../assets/images/icons/menu.svg";
 
 const Header = () => {
   return (
@@ -11,20 +12,23 @@ const Header = () => {
         <hr className="z-1 absolute ml-auto mt-9 h-px w-1/3 pt-5" />
       </div>
 
-      <ul className="hidden sm:flex flex-row justify-center bg-opacity-60 uppercase tracking-slimmer backdrop-blur-xl backdrop-filter">
-        <li className="px-5">
+      <ul className="hidden md:flex flex-row justify-center items-center bg-opacity-60 uppercase tracking-slimmer backdrop-blur-xl backdrop-filter md:h-28">
+        <li className="mx-5 ">
           <span className="pr-3 font-semibold">00</span>Home
         </li>
-        <li className="px-5">
+        <li className="mx-5">
           <span className="pr-3 font-semibold">01</span>Destination
         </li>
-        <li className="px-5">
+        <li className="mx-5">
           <span className="pr-3 font-semibold">02</span>Crew
         </li>
-        <li className="px-5">
+        <li className="mx-5">
           <span className="pr-3 font-semibold">03</span>Technology
         </li>
       </ul>
+      <div className="flex md:hidden">
+        <img src={Menu} />
+      </div>
     </nav>
   );
 };
